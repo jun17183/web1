c92478025417560d663818316d674bdddbd32ebe
jun17183 <64164782+jun17183@users.noreply.github.com>
2020-09-21 00:47:54 +0900

    Add files via upload
    <basic html + css + javascript>
    add) day night button + jquery / mobile

--- colors.js
+++ colors.js
@@ -1,19 +1,12 @@
 var body = {
 	setColor : function(color) 
 		{$('body').css('color', color)},
-		/*{document.querySelector('body').style.color 		= color;},*/
 	setBackgroundColor : function(color) 
 		{$('body').css('backgroundColor', color)}
-		/*{document.querySelector('body').style.background 	= color;}*/
 }
 
 var links = {
 	setColor : function(color) {
-		/*var aList = document.querySelectorAll('a');
-		
-		for(i = 0; i < aList.length; i++) {
-			aList[i].style.color = color;
-		}*/
 		$('a').css('color', color);
 	}
 }
@@ -23,34 +16,14 @@ function dayNightHandler(self) {
 		body.setBackgroundColor('#222423');
 		body.setColor('white');
 		
-		if (self.id === 'night_day_button') {
-			$('#night_day_button').val('day');
-			$('#night_day_button2').val('day');
-			/*document.querySelector('#night_day_button').value = 'day';
-			document.querySelector('#night_day_button2').value 	= 'day';*/
-		} else if (self.id === 'night_day_button2') {
-			$('#night_day_button').val('day');
-			$('#night_day_button2').val('day');
-			/*document.querySelector('#night_day_button').value	= 'day';
-			document.querySelector('#night_day_button2').value 	= 'day';*/
-		}
+		$('.daynightbutton').val('day');
 		
 		links.setColor('powderblue');
 	} else {
 		body.setBackgroundColor('white');
 		body.setColor('black');
 		
-		if (self.id == 'night_day_button') {
-			$('#night_day_button').val('night');
-			$('#night_day_button2').val('night');
-			/*document.querySelector('#night_day_button').value 	= 'night';
-			document.querySelector('#night_day_button2').value 	= 'night';*/
-		} else if (self.id == 'night_day_button2') {
-			$('#night_day_button').val('night');
-			$('#night_day_button2').val('night');
-			/*document.querySelector('#night_day_button').value 	= 'night';
-			document.querySelector('#night_day_button2').value 	= 'night';*/
-		}
+		$('.daynightbutton').val('night');
 		
 		links.setColor('black');
 	}
